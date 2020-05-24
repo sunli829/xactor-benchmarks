7ee4a356885b6d9d5419fe74c31820ee3cc0e275
sunny-g <sunny.gonna@gmail.com>
2020-05-23 20:01:56 -0500

    shorten the benches by default

--- src/benches.rs
+++ src/benches.rs
@@ -1,31 +1,38 @@
-use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
-use std::fmt;
-use std::time::Duration;
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
+use std::{fmt, time::Duration};
 use xactor_benchmarks::{actix_test, xactor_test, Result as BenchResult, Spec};
 
-criterion_group!(benches, bench);
+criterion_group!(benches, bench_actix, bench_xactor);
 criterion_main!(benches);
 
-fn bench(c: &mut Criterion) {
-    let tests = gen_tests();
-    let mut group = c.benchmark_group("actor_tests");
+fn bench_actix(c: &mut Criterion) {
+    let tests = gen_tests(Some(2));
 
-    for (i, spec) in tests.into_iter().enumerate() {
-        group.bench_with_input(BenchmarkId::new("actix", i), &spec, |b, spec| {
-            b.iter(|| actix_test::run(spec))
+    let mut group = c.benchmark_group("actix");
+    for spec in tests.into_iter() {
+        group.bench_with_input(BenchmarkId::from_parameter(&spec), &spec, |b, spec| {
+            b.iter(|| actix_test::run(black_box(spec)))
         });
+    }
+    group.finish();
+}
 
-        group.bench_with_input(BenchmarkId::new("xactor", i), &spec, |b, spec| {
+fn bench_xactor(c: &mut Criterion) {
+    let tests = gen_tests(Some(2));
+
+    let mut group = c.benchmark_group("xactor");
+    for spec in tests.into_iter() {
+        group.bench_with_input(BenchmarkId::from_parameter(&spec), &spec, |b, spec| {
             // See https://github.com/async-rs/async-std/issues/770#issuecomment-633011171
-            b.iter(|| smol::run(async { xactor_test::run(spec).await }))
+            b.iter(|| smol::run(async { xactor_test::run(black_box(spec)).await }))
         });
     }
     group.finish();
 }
 
 // Generate the benchmark specifications
-fn gen_tests() -> Vec<Spec> {
-    let max = num_cpus::get() as u32;
+fn gen_tests(max: Option<u32>) -> Vec<Spec> {
+    let max = max.unwrap_or(num_cpus::get() as u32 + 1);
     let mut v = Vec::new();
     for procs in 1..max {
         for msgs in 1..max {
